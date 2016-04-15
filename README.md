5647f45e81ab5959ac72830348c586d2d0401e7c
FatShotty <fat@fatshotty.net>
2016-04-15 12:36:22 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,101 @@
-# lite-log.js
+# Lite-Log
+
+Lite-Log is a simple easy-to-use library for managing all log line in console
+
+
+### Here is an example
+
+```js
+  // STATIC METHODS
+  Logger("Simple line with an object", {key: "value"});  // alias to `Logger.log`
+  Logger.i("this is a test for static instance:", "this is another argument as string", {key: "argument as object"});  // alias to `Logger.info`
+  Logger.w("this is a test for static instance:", "this is another argument as string", {key: "this is an argument as object"});  // alias to `Logger.warn`
+  Logger.mute(true);
+  Logger.i("this line will be ignored");  // alias to `Logger.info`
+  Logger("this line will be ignored");    // alias to `Logger.log`
+
+  // INSTANCE METHOD
+  var Log = new Logger("ModuleName", true);
+  Log.i("this is a test:", "this is another argument as string", {key: "argument as object"});  // alias to `Log.info`
+  Log.w("this is a test:", "this is another argument as string", {key: "this is an argument as object"});  // alias to `Log.warn`
+  Log.mute(true);
+  Log.i("this line will be ignored");  // alias to "Log.info"
+
+```
+
+
+### Usage
+
+##### Logger(args...)
+Simply log all passed arguments. It is the same of `console.log`
+
+
+##### Logger.e(args...)  or  Logger.error(args...)
+
+Log all passed arguments as per `console.error`
+
+
+##### Logger.w(args...)  or  Logger.warn(args...)
+
+Log all passed arguments as per `console.warn`
+
+
+##### Logger.i(args...)  or  Logger.info(args...)
+
+Log all passed arguments as per `console.info`
+
+
+##### Logger.l(args...)  or  Logger.log(args...)
+
+Log all passed arguments as per `console.log`
+
+
+##### Logger.d(args...)  or  Logger.debug(args...)
+
+Log all passed arguments as per `console.debug`
+
+
+##### Logger.mute(boolean)
+
+Mute/Unmute the logger. Once logger has been muted, it will log nothing
+
+
+##### new Logger(modulename, showline)
+
+Create a new Logger instance.
+Arguments:
+`modulename` (String/Boolean): it is the name of the module just invoked the Logger.
+If `String` it represent exactly the name of the module.
+If `True` it will get the name of the module from the stacktrace.
+If `False` it will not log the module name.
+
+`showline` (Boolean): it enables/disables the `linenumber` in the log.
+
+```js
+var Log_1 = new Logger("ModuleName_1", false);
+var Log_2 = new Logger("ModuleName_2", false);
+Log_1.i("Log my argument");
+Log_2.i("Log my argument");
+// [ModuleName_1] Log my argument
+// [ModuleName_2] Log my argument
+```
+
+
+```js
+var Log_1 = new Logger("ModuleName_1", true);
+var Log_2 = new Logger("ModuleName_2", false);
+Log_1.i("Log my argument");
+Log_2.i("Log my argument");
+// [ModuleName_1:3] Log my argument
+// [ModuleName_2] Log my argument
+```
+
+```js
+// this code is in the `main.js` file
+var Log_1 = new Logger(true, true);
+var Log_2 = new Logger(false, true);
+Log_1.i("Log my argument");
+Log_2.i("Log my argument");
+// [main:3] Log my argument
+// Log my argument
+```
---
+++ logger.js
@@ -0,0 +1,145 @@
+var Logger = (function(){
+
+  var _mute = false;
+
+  function Logger(module, showline){
+
+    if ( !(this instanceof Logger) ) {
+      return Logger.l.apply( null, arguments );
+    }
+
+    if ( typeof module == 'string') {
+      this.modulename = module;
+    } else if ( typeof module == 'boolean') {
+
+      if ( module ) {
+        // get the module name from stacktrace
+        this.modulename = _modulenamefromstack();
+      } else {
+        // No module name in the log strings
+        this.modulename = "";
+      }
+
+    } else {
+      Logger.w("Invalid 'module name' for logger.", "use 'default'");
+      this.modulename = "*";
+    }
+
+    this.showline = !!showline;
+
+  }
+
+  Logger.prototype = {
+
+    e: _wrap("error"),
+    w: _wrap("warn"),
+    i: _wrap("info"),
+    l: _wrap("log"),
+    d: _wrap("debug"),
+
+    mute: function(mute) {
+      if ( this instanceof Logger ) {
+        this._mute = !!mute;
+        return this;
+      } else {
+        _mute = !!mute;
+        return Logger;
+      }
+    }
+
+  };
+
+  function _wrap(level) {
+    return function(args) {
+      args = Array.prototype.slice.call(arguments, 0);
+      if ( this instanceof Logger ) {
+        if ( this._mute ) return false;
+        args.unshift(this.showline);
+        args.unshift(this.modulename);
+      } else {
+        if ( _mute ) return false;
+        args.unshift(false);  // showline
+        args.unshift(undefined); // modulename
+      }
+      args.unshift(level);
+      return Logger._write.apply( null, args);
+    };
+
+  }
+
+
+  function _getstack() {
+    var
+      error = null;
+    if ( window.Error ) {
+      error = new Error("nothing");
+    } else {
+      try{ __unexisting_function_42__() } catch(e) { error = e; }
+    }
+    return error.stack;
+  }
+
+  function _getlinenumber() {
+    var
+      stacktrace = _getstack(),
+      stacktracelines = stacktrace.split("\n"),
+      line = stacktracelines[5],
+      matches = line.match(/([^\/]+$)/),
+      match = matches[1],
+      parts = match.split(":"),
+      part = parts[1];
+
+    return part;
+
+  }
+
+  function _modulenamefromstack() {
+
+    var
+      stacktrace = _getstack(),
+      stacktracelines = stacktrace.split("\n"),
+      line = stacktracelines[4],
+      matches = line.match(/([^\/]+$)/),
+      match = matches[1],
+      parts = match.split(":"),
+      part = parts[0],
+      fileparts = part.split(".");
+
+      fileparts.pop();
+      return fileparts.join(".");
+  };
+
+  Logger._write = function(level, module, showline, args) {
+
+    args = Array.prototype.slice.call(arguments, 3);
+
+    if ( module ) {
+      if ( showline ) {
+        var line = _getlinenumber();
+        args[0] = "[" + module + ":" + line + "] " + args[0];
+      } else {
+        args[0] = "[" + module + "] " + args[0];
+      }
+    }
+
+    if ( window.console ){
+      return console[ level ].apply( console, args );
+    } else {
+      return false;
+    }
+  };
+
+  Logger.prototype.error = Logger.prototype.e;
+  Logger.prototype.warn = Logger.prototype.w;
+  Logger.prototype.info = Logger.prototype.i;
+  Logger.prototype.log = Logger.prototype.l;
+  Logger.prototype.debug = Logger.prototype.d;
+
+  for ( fn in Logger.prototype ) {
+    if ( Logger.prototype.hasOwnProperty(fn) ) {
+      Logger[ fn ] = Logger.prototype[ fn ];
+    }
+  }
+
+  return Logger;
+})();
